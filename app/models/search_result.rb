13123b1231a6c7fdf0c7af0c3e37a7095fa768b0
Ale Miralles <alemiralles@hey.com>
2024-03-10 17:36:31 -0300

    Add collector for Open Library and extract common methods to base class

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,7 +1,9 @@
 class SearchResult < ApplicationRecord
   before_save :normalize_source
+  before_save :normalize_isbn
 
   validates :isbn, :source, :payload, presence: true
+  validate :isbn_length
 
   private
 
@@ -9,5 +11,19 @@ class SearchResult < ApplicationRecord
     return unless attribute_present?("source")
 
     self.source = source.downcase.gsub(" ", "_")
+  end
+
+  def normalize_isbn
+    return unless attribute_present?("isbn")
+
+    self.isbn = isbn.gsub(/\D/, "")
+  end
+
+  def isbn_length
+    return unless isbn.present?
+
+    unless [ 10, 13 ].include? isbn.length
+      errors.add(:isbn, "length must be either 10 or 13 numbers")
+    end
   end
 end
